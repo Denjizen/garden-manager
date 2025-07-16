a793d6da328d49a24f1147cccd02a9650a5cd060
Cameron <Cameron.Burke.8@hotmail.com>
2025-07-16 09:55:14 +0100

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://github.com/Denjizen/garden-manager.git/sw.js', { scope: 'https://github.com/Denjizen/garden-manager.git/' })})}
